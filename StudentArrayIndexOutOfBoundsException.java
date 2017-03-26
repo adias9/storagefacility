6c0e670c371455cd3f6aed40fb09ec6e9525a587
adias9 <andreasdias9@gmail.com>
2017-03-26 13:17:09 -0400

    First commit with initial code

---
+++ StudentArrayIndexOutOfBoundsException.java
@@ -0,0 +1,4 @@
+public class StudentArrayIndexOutOfBoundsException extends ArrayIndexOutOfBoundsException {
+	public StudentArrayIndexOutOfBoundsException() { }
+	public StudentArrayIndexOutOfBoundsException(String explanation) { super(explanation); }
+}
